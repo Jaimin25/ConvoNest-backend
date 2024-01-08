a3311bd5327f082b5d9410b84618a4cc4aa10d24
jaimin25 <jaiminchovatia25123@gmail.com>
2024-01-08 23:26:28 +0530

    try2

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ const server = createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "*",
+    origin: ["http://localhost:3000", "https://convonest.vercel.app"],
     methods: ["GET", "POST"],
     credentials: true,
   },
